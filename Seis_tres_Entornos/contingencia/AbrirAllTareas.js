7f7f141cefbc977b8a8cfb9db8fcdb40f5c9c3f8
natisbar <48846184+natisbar@users.noreply.github.com>
2021-01-21 12:12:27 -0500

    Update AbrirAllTareas.js

--- Seis_tres_Entornos/contingencia/AbrirAllTareas.js
+++ Seis_tres_Entornos/contingencia/AbrirAllTareas.js
@@ -1,6 +1,6 @@
 var aTags = document.getElementsByTagName("a");
-var searchText = "TAREA";
-var searchText2 = "ASSIGNMENT";
+var searchText = "ICONLARGEACTIVITYICON";
+var searchText2 = "iconlarge activityicon";
 var found = [];
 var found2 = [];
 var found3 = [];
@@ -9,27 +9,26 @@ var contar2 = 0;
 var contar3 = 0;
 
 for (var i = 0; i < aTags.length; i++) {
-if (aTags[i].getElementsByTagName("span")[1]) {
-	found[contar] = aTags[i];
-	contar++;
-}
+    if (aTags[i].getElementsByTagName("img")[0]) {
+        found[contar] = aTags[i];
+        contar++;
+    }
 }
 
 for (var i = 0; i < found.length; i++) {
-if (found[i].childNodes.length==2 && found[i].childNodes[1].childNodes.length==2) {
-	found2[contar2] = found[i];
-	contar2++;
-}
+        found2[contar2] = found[i].childNodes[0].className;
+        contar2++;
 }
 
 for (var i = 0; i < found2.length; i++) {
-if (found2[i].childNodes[1].childNodes[1].innerText.replace(/ /g,"").toUpperCase() == searchText || found2[i].childNodes[1].childNodes[1].innerText.replace(/ /g,"").toUpperCase() == searchText2) {
-	found3[contar3] = found2[i];
-	contar3++;
-}
+    if (found2[i] == searchText2) {
+        found3[contar3] = found[i];
+        contar3++;
+    }
 }
 
-for (var i = 0; i < found3.length; i++) {
+
+for (var i = found3.length-8; i < found3.length; i++) {
     var textedit = "course/modedit.php?update=";
     var m_edicion = found3[i].getAttribute("href");
     var fin = m_edicion.indexOf("mod");
