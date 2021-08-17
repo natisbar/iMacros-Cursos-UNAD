c91cb0da32aadd4d333408a5e7c40cb2d333c137
natisbar <48846184+natisbar@users.noreply.github.com>
2021-08-17 11:26:45 -0500

    Entorno inicial
    Se ajusta entorno inicial por ajuste en id de bloque revisor de la agenda

--- Rubrica_No_Aceptado/prub.js
+++ Rubrica_No_Aceptado/prub.js
@@ -10,6 +10,3 @@ for(i=2; i<=contar+1; i++){
     assignmentField.value = 1;
     document.getElementById("bguarda_"+i).click();
 }
-
-
-var contar = 0; for(var i=2; i<=200; i++){if(document.getElementById("res_"+i)!=null){contar++;}}for(i=2; i<=contar+1; i++){let assignmentField = document.getElementById("res_"+i);assignmentField.value = 0;document.getElementById("bguarda_"+i).click();}
